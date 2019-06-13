7488bdfce220e1b222d97d40206d80bf8498265b
Amir Hossein Abdolkhalegh <ah.abdolkhalegh@gmail.com>
2019-06-13 23:12:40 +0430

    add convert.toJs function with all its tests

--- src/converters.js
+++ src/converters.js
@@ -69,40 +69,65 @@ function toSass(t) {
 
 function toJs() {
   return function (sassValue) {
-    const type = getTypeOfSassValue(sassValue)
-    switch (type) {
-      case "number":
-      case "string":
-      case "color":
-      case "boolean":
-      case "list":
-      case "map":
-      case "null":
-      default:
+    return toJsValue(sassValue)
+  }
+
+  function toJsValue(sassValue) {
+    switch (sassValue.constructor.name) {
+      case "SassNumber":
+        return toNumber(sassValue)
+      case "SassString":
+        return toString(sassValue)
+      case "SassColor":
+        return toColor(sassValue)
+      case "SassBoolean":
+        return toBoolean(sassValue)
+      case "SassList":
+        return toArray(sassValue)
+      case "SassMap":
+        return toObject(sassValue)
+      case "SassNull":
+        return null
     }
   }
 
-  function getTypeOfSassValue() {
-
-  }
-
   function toNumber(value) {
-
+    if (value.getUnit()) {
+      return {value: value.getValue(), unit: value.getUnit()}
+    }
+    return value.getValue()
   }
 
   function toString(value) {
+    return value.getValue()
+      .replace(/^"/, "")
+      .replace(/"$/, "")
+  }
 
+  function toColor(value) {
+    return {
+      red: value.getR(),
+      green: value.getG(),
+      blue: value.getB(),
+      alpha: value.getA(),
+    }
   }
 
   function toBoolean(value) {
-
+    return value === sassTypes.Boolean.TRUE
   }
 
   function toArray(value) {
-
+    return [...Array(value.getLength()).keys()]
+      .map(i => toJsValue(value.getValue(i)))
   }
 
   function toObject(value) {
-
+    return [...Array(value.getLength()).keys()]
+      .reduce((obj, _, i) => {
+        obj[value.getKey(i).getValue()] = toJsValue(value.getValue(i))
+        return obj
+      }, {})
   }
+
 }
--- test/converters.test.js
+++ test/converters.test.js
@@ -1,7 +1,7 @@
 import {convert} from "../src/converters";
 import {types} from "node-sass";
 
-describe("Convert to SASS value", () => {
+describe("convert.toSass", () => {
   test("should convert the given number to SASS number", () => {
     const value = 42
     const got = convert.toSASS(value)
@@ -85,4 +85,111 @@ describe("Convert to SASS value", () => {
 
     expect(obj).toStrictEqual(value)
   })
+})
+describe("convert.toJs", () => {
+  test("should convert the given SASS Number to JS number", () => {
+    const value = new types.Number(42)
+    const got = convert.toJS(value)
+    expect(got).toBe(value.getValue())
+  })
+
+  test("should convert the given SASS Number with unit to JS Object", () => {
+    const value = new types.Number(42, "rem")
+    const got = convert.toJS(value)
+    expect(got).toStrictEqual({value: 42, unit: "rem"})
+  })
+
+  test.each([
+    [new types.String("dummy"), "dummy"],
+    [new types.String("\"dummy string\""), "dummy string"],
+  ])("should convert SASS String%p to JS string %p", (value, expected) => {
+    const got = convert.toJS(value)
+    expect(got).toBe(expected)
+  })
+
+  test.each([
+    [types.Boolean.TRUE, true],
+    [types.Boolean.FALSE, false],
+  ])("should convert SASS Boolean %p to JS %p", (value, expected) => {
+    const got = convert.toJS(value)
+    expect(got).toBe(expected)
+  })
+
+  test.each([
+    [new types.Color(10, 20, 30), {red: 10, green: 20, blue: 30, alpha: 1}],
+    [new types.Color(10, 20, 30, .2), {red: 10, green: 20, blue: 30, alpha: .2}],
+  ])("should convert SASS Color%P to JS Object %p", (value, expected) => {
+    const got = convert.toJS(value)
+    expect(got).toStrictEqual(expected)
+  })
+
+  test("should convert SASS NULL to null", () => {
+    const got = convert.toJS(types.Null.NULL)
+    expect(got).toBeNull()
+  })
+
+  test.each([
+    [[1, 2, 3].reduce((list, v, i) => {
+      list.setValue(i, new types.Number(v))
+      return list
+    }, new types.List(3)), [1, 2, 3]],
+  ])
+  ("should convert SASS List%p to JS %p", (value, expected) => {
+    const got = convert.toJS(value)
+    expect(got).toStrictEqual(expected)
+  })
+
+  test("should convert complex SASS List to JS Array", () => {
+    const expected = [1, ["a", "b"], {red: 0, green: 0, blue: 0, alpha: 1}]
+    let list = new types.List(3)
+    let innerList = new types.List(2)
+
+    list.setValue(0, new types.Number(1))
+    list.setValue(1, new types.List(2))
+    innerList.setValue(0, new types.String("a"))
+    innerList.setValue(1, new types.String("b"))
+    list.setValue(1, innerList)
+    list.setValue(2, new types.Color(0, 0, 0, 1))
+
+    const got = convert.toJS(list)
+    expect(got).toStrictEqual(expected)
+  })
+
+  test("should convert SASS Map to JS object", () => {
+    const expected = {name: "name", age: "3"}
+    const map = Object.keys(expected).reduce((map, v, i) => {
+      map.setKey(i, new types.String(v))
+      map.setValue(i, new types.String(expected[v]))
+      return map
+    }, new types.Map(2))
+    const got = convert.toJS(map)
+    expect(got).toStrictEqual(expected)
+  })
+
+  test("should convert complex SASS Map to JS object", () => {
+    const expected = {name: "name", age: 3, hair: {color: "red"}, lang: ["en", "fr"]}
+    const map = new types.Map(4)
+
+    map.setKey(0, new types.String("name"))
+    map.setValue(0, new types.String("name"))
+
+    map.setKey(1, new types.String("age"))
+    map.setValue(1, new types.Number(3))
+
+    let hair = new types.Map(1)
+    hair.setKey(0, new types.String("color"))
+    hair.setValue(0, new types.String("red"))
+    map.setKey(2, new types.String("hair"))
+    map.setValue(2, hair)
+
+    let lang = new types.List(2)
+    lang.setValue(0, new types.String("en"))
+    lang.setValue(1, new types.String("fr"))
+    map.setKey(3, new types.String("lang"))
+    map.setValue(3, lang)
+
+    const got = convert.toJS(map)
+    expect(got).toStrictEqual(expected)
+  })
+
 })
