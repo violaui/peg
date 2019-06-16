727bc5dd355d20c102704cab2fea4b5f6e34fca5
amirhosseinab <ah.abdolkhalegh@gmail.com>
2019-06-16 13:08:45 +0430

    add banner header to the output files

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,4 @@
+const pkg = require("./package.json")
 module.exports = function (api) {
   api.cache.never();
 
@@ -11,6 +12,9 @@ module.exports = function (api) {
   ];
   const plugins = [
     "@babel/plugin-proposal-object-rest-spread",
+    ["add-header-comment", {
+      header: [`${pkg.name} v${pkg.version} | ${pkg.repository} | ${pkg.license} License\nAuthor: ${pkg.author.name} | ${pkg.author.url}`]
+    }]
   ];
   return {
     sourceType: "unambiguous",
